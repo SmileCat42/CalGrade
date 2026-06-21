eab2d68bc93990c13e3dc91ccede8f62accbead5
Golfring <golfring.dove@hotmail.com>
2026-06-21 16:43:03 +0700

    add grade box and detail label

--- main.py
+++ main.py
@@ -3,6 +3,38 @@ from tkinter import ttk
 import json
 
 
+def update_grade():
+
+    selected_item = table.selection()
+
+    if not selected_item:
+        return
+
+    table.set(
+        selected_item[0],
+        "grade",
+        grade_var.get()
+    )
+
+def on_select(event):
+    grade_combo.set("กรุณาเลือกเกรด")
+    selected_item = table.selection()
+
+    if not selected_item:
+        return
+
+    values = table.item(selected_item[0], "values")
+
+    code = values[0]
+    name = values[1]
+
+    selected_subject_label.config(
+        text=f"วิชาที่เลือก: {code} {name}"
+    )
+
+    grade_combo.config(state="readonly")
+
+
 window = tk.Tk()
 
 window.title("โปรแกรมบันทึกผลการเรียน")
@@ -42,6 +74,44 @@ for subject in subjects:
             subject["credit"],
             subject["grade"]
         ))
+
+grade_var = tk.StringVar()
+
+grade_combo = ttk.Combobox(
+    window,
+    textvariable=grade_var,
+    values=["", "A", "B+", "B", "C+", "C", "D+", "D", "F"],
+    state="readonly"
+)
+
+grade_combo.set("กรุณาคลิกเลือกวิชา")
+
+update_button = tk.Button(
+    window,
+    text="อัปเดตเกรด"
+)
+
+update_button = tk.Button(
+    window,
+    text="อัปเดตเกรด",
+    command=update_grade
+)
+
+selected_subject_label = tk.Label(
+    window,
+    text="ยังไม่ได้เลือกวิชา",
+    font=("Arial", 11)
+)
+
+table.bind("<<TreeviewSelect>>", on_select)
+
+
+grade_combo.pack(pady=5)
+grade_combo.config(state="disabled") 
+
+selected_subject_label.pack()
+
+update_button.pack(pady=5)
 
 table.pack(fill="both", expand=True, padx=20, pady=20)
 
